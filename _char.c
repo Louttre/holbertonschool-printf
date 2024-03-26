eaae8c7eb094dff28e502d26b9a6ad046a5d883e
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-27 00:11:20 +0100

    Update _char.c

--- _char.c
+++ _char.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _char - print a char
+ * @args: list of arguments
+ * @count: number of character
+ * Return: count + 1
+ */
 int _char(va_list args, int count)
 {
 	char c = va_arg(args, char);
