851a94281f5ef515312cb1b4a234f5aca219a68e
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-27 00:19:48 +0100

    Update _string.c

--- _string.c
+++ _string.c
@@ -3,7 +3,11 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <string.h>
-
+/**
+ * _string - print a string
+ * @args: string to print
+ * Return: count + lenght of the printed string
+ */
 int _string(va_list args, int count)
 {
         char *s = va_arg(args, char *);
