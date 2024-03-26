528ddbe6809110aaba00b1739c15b8da3488e5e3
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-03-26 15:40:17 +0100

    Update main.h

--- main.h
+++ main.h
@@ -12,5 +12,5 @@ int _printf(const char *format, ...);
 int _string(va_list, int);
 int _int(va_list, int);
 char *itoa(int i);
-int _char(va_list args, int count);
+int _char(va_list, int);
 #endif
