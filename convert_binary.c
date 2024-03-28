17fdfc9f62e10b8f7ff11687a95caf154d2b629d
root <seth.rueda@gmail.com>
2024-03-28 19:09:51 +0000

    UPDATE

--- convert_binary.c
+++ convert_binary.c
@@ -14,19 +14,36 @@ char *convert_binary(int num)
 	unsigned int i = 0;
 	char *s;
 	int lennum;
-	
+	int checksign = 0;
+
 	if (num < 0)
+	{
 		num = -num;
+		checksign = 1;
+	}
 	lennum = num;
 	while (lennum > 0)
 	{
 		lennum /= 2;
 		i++;
 	}
-	s = malloc(sizeof(char) * (i + 1));
+	if (checksign)
+	{
+		s = malloc(sizeof(char) * (i + 2));
+		i += 1;
+	}
+	else	
+		s = malloc(sizeof(char) * (i + 1));
 	s[i] = '\0';
+	if (s == NULL)
+		return (NULL);
 	while (i > 0)
 	{
+		if (checksign && i == 1)
+		{
+			s[i - 1] = '1';
+			break;
+		}
 		s[i - 1] = num % 2 + '0';
 		num /= 2;
 		i--;
